4eb3c86c8746508c6da5ac92311d513e32f59b5d
fahriui23 <trias.fahri@ui.ac.id>
2024-09-10 08:36:41 +0700

    pws configuration

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j-1m!4w%!qy&%!48hk27wmiz#25lrv%p09a91fla48h+x1gu1t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","trias-fahri-wandaberas.pbp.cs.ui.ac.id"]
 
 
 # Application definition
