f90eb7a78f3de3f71b8e61082e89ff7749445845
fahriui23 <trias.fahri@ui.ac.id>
2024-09-15 19:58:36 +0700

    XML and JSON

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect   
+from django.shortcuts import render, redirect  
+from django.http import HttpResponse
+from django.core import serializers 
 from main.forms import ProductForm
 from main.models import Product
 
@@ -25,3 +27,11 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
